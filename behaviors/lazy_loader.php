2f237998df570c6ce23eec7733d349b37f03dc1c
rafaelbandeira3 <rafaelbandeira3@gmail.com>
2008-11-14 14:19:36 -0200

    Adding LazyLoaderBehavior and test case for it

---
+++ behaviors/lazy_loader.php
@@ -0,0 +1,138 @@
+<?php
+/*
+ * LazyLoaderBehavior
+ * Enables easy access to related data through a Lazy Load¹ like interface,
+ * mapping methods named as "get<AssociatedModelName>" or "get_<associated_model_name>",
+ * to fetch related data and return. Can receive a string as first parameter to set the find type² to be 
+ * used in the operation. If the associated model to be used in the operation is related as
+ * "belongsTo" or "hasOne", a boolean TRUE can be passed as first or second param to force method to return
+ * the associated model instance setted with the related record data.
+ * ¹ - http://en.wikipedia.org/wiki/Lazy_loading
+ * ² - http://cakebaker.42dh.com/2008/09/23/an-alternative-way-to-define-custom-find-types/
+ *
+ * Usage:
+ * Notice that the method requires the Model to be instantiated with an exitant record id, and the
+ * related model to be binded during the process.
+ *
+ * Project belongsTo Company
+ * Project hasOne Owner
+ * Project hasMany Task
+ * Project hasAndBelongsToMany User
+ * Projects hasMany FeaturesProblemsAndBugs
+ *
+ * $Project->getCompany(); // returns data of the related Company
+ * $Project->getOwner(); // returns data of the related Owner
+ * $Project->getTasks(); // returns related tasks list
+ * $Project->getUsers(); // returns related users list
+ * $Project->getTasks('all'); // returns related tasks data
+ * $Project->getTasks('closed'); // returns related tasks fetched by a Task::find('closed') operation
+ * $Project->getCompany(true); // returns Company model instance setted with related record data
+ * $Project->getTask(); // error as Project has many Task not just one
+ * $Project->get_tasks(); // works too
+ * $Project->get_features_problems_and_bugs(); // works too
+ *
+ * Although not recommended can also be through the behavior method:
+ * - notice that it would break extensibility and flexibility of overriding to customization on Model side.
+ * $Project->lazyLoad('Company');
+ * $Project->lazyLoad('Tasks');
+ *
+ * LazyLoaderBehavior. Easy things easy, but not less smart.
+ * RafaelBandeira <rafaelbandeira3(at)gmail(dot)com>
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @version 0.1;
+ */
+
+class LazyLoaderBehavior extends ModelBehavior {
+	public $mapMethods = array('/^get(_[a-z0-9_]+)|([a-z0-9]+)/' => 'lazyLoad');
+
+	public function &lazyLoad(&$Model, $association, $type = null, $instance = false) {
+		if (!$Model->exists()) {
+			throw new Exception('Model must be associated with an existing record');
+			return false;
+		}
+		$association = $this->_getAssociation($Model, $association);
+		if ($association === false) {
+			throw new InvalidArgumentException('Model must be associated with the model intended to be loaded');
+			return false;
+		} elseif ($type === true) {
+			$type = null;
+			$instance = true;
+		}
+
+		if (empty($type)) {
+			if ($association['type'] === 'hasAndBelongsToMany' || $association['type'] === 'hasMany') {
+				$type = 'list';
+			} else {
+				$type = 'first';
+			}
+		}
+		$keys = array('conditions' => true, 'order' => true, 'group' => true);
+		if ($type !== 'list') {
+			$keys['fields'] = true;
+		}
+		$query = array_intersect_key($association['association'], $keys);
+		$query['recursive'] = -1;
+		if (!isset($query['conditions'])) {
+			$query['conditions'] = array();
+		} elseif (!is_array($query['conditions'])) {
+			$query['conditions'] = array($query['conditions']);
+		}
+
+		$_Model =& $Model->{$association['alias']};
+		if ($association['type'] === 'hasAndBelongsToMany') {
+			$Link =& $Model->{$association['association']['with']};
+
+			$foreignKey = $Link->escapeField($association['association']['foreignKey']);
+			$associationKey = $_Model->escapeField();
+			$associationForeignKey = $Link->escapeField($association['association']['associationForeignKey']);
+
+			$query['conditions'][] = array($foreignKey => $Model->id);
+			$query['joins'][] = array(
+				'alias' => $Link->alias,
+				'table' => $Link->getDataSource()->fullTableName($Link),
+				'type' => 'LEFT',
+				'conditions' => "{$associationForeignKey} = {$associationKey}"
+			);
+		} elseif ($association['type'] === 'hasOne' || $association['type'] === 'hasMany') {
+			$foreignKey = $_Model->escapeField($association['association']['foreignKey']);
+			$query['conditions'][] = array($foreignKey => $Model->id);
+		} else {
+			$query['conditions'][] = array($_Model->escapeField() => $Model->field($association['association']['foreignKey']));
+		}
+
+		if ($instance && ($association['type'] === 'belongsTo' || $association['type'] === 'hasOne')) {
+			$_Model->set($_Model->find($type, $query));
+			return $_Model;
+		} else {
+			$return = $_Model->find($type, $query);
+			return $return;
+		}
+	}
+
+	protected function _getAssociation(&$Model, $association) {
+		if (preg_match('/^get/', $association)) {
+			$association = substr($association, 3);
+		}
+		$associateds = $Model->getAssociated();
+		if (!empty($associateds)) {
+			$associated = Inflector::camelize($association);
+			$alias = Inflector::classify($association);
+			
+			if (isset($associateds[$alias])) {
+				$type = $associateds[$alias];
+				$association = $Model->{$type}[$alias];
+				$valid = (
+					($type === 'belongsTo' || $type === 'hasOne') ||
+					Inflector::pluralize($alias) === $associated
+				);
+				if ($valid) {
+					return compact('alias', 'type', 'association');
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ tests/lazy_loader.test.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * Unit test for LazyLoaderBehavior v0.1.
+ *
+ * LazyLoaderBehavior. Easy things easy, but not less smart.
+ * RafaelBandeira <rafaelbandeira3(at)gmail(dot)com>
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ */
+
+App::import('Behavior', 'LazyLoader');
+include CAKE_TESTS . 'cases' . DS . 'libs' . DS . 'model' . DS . 'models.php';
+
+class LazyLoaderBehaviorTest extends CakeTestCase {
+
+	var $fixtures = array(
+		'core.portfolio', 'core.item', 'core.items_portfolio',
+		'core.syfile', 'core.image', 'core.message',
+		'core.thread', 'core.bid', 'core.project'
+	);
+	
+	function start() {
+		parent::start();
+		$this->Portfolio = ClassRegistry::init('Portfolio');
+		$this->Item = ClassRegistry::init('Item');
+		$this->Syfile = ClassRegistry::init('Syfile');
+		$this->Image = ClassRegistry::init('Image');
+		$this->Message = ClassRegistry::init('Message');
+		$this->Thread = ClassRegistry::init('Thread');
+
+		$this->Portfolio->Behaviors->attach('LazyLoader');
+		$this->Item->Behaviors->attach('LazyLoader');
+		$this->Syfile->Behaviors->attach('LazyLoader');
+		$this->Image->Behaviors->attach('LazyLoader');
+		$this->Message->Behaviors->attach('LazyLoader');
+		$this->Thread->Behaviors->attach('LazyLoader');
+		
+		$this->Image->bind('Syfile', array('hasOne'));
+		$this->Thread->bind('Message', array('hasMany'));
+	}
+
+	function testBelongsTo() {
+		$this->Syfile->id = 1;
+		$result = $this->Syfile->getImage();
+		$expected = array('Image' => array('id' => 1, 'name' => 'Image 1'));
+		$this->assertEqual($result, $expected);
+	}
+
+	function testBelongsToLoadInstance() {
+		$this->Syfile->id = 1;
+		$result = $this->Syfile->getImage(true);
+		$this->assertIsA($result, 'Image');
+	}
+	
+	function testHasOne() {
+		$this->Image->id = 1;
+		$result = $this->Image->getSyfile();
+		$expected = array('Syfile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
+		$this->assertEqual($result, $expected);
+	}
+	
+	function testHasOneLoadInstance() {
+		$this->Image->id = 1;
+		$result = $this->Image->getSyfile(true);
+		$this->assertIsA($result, 'Syfile');
+	}
+	
+	function testHasAndBelongsToMany() {
+		$this->Portfolio->id = 1;
+		$result = $this->Portfolio->getItems();
+		$expected = array(1 => 'Item 1', 3 => 'Item 3', 4 => 'Item 4', 5 => 'Item 5');
+		$this->assertEqual($result, $expected);
+	}
+	
+	function testHasMany() {
+		$this->Thread->id = 1;
+		$result = $this->Thread->getMessages();
+		$expected = array(1 => 'Thread 1, Message 1');
+		$this->assertEqual($result, $expected);
+	}
+	
+	function testAlternativeFindType() {
+		$this->Portfolio->id = 1;
+		$result = $this->Portfolio->getItems('all');
+		$expected = array(
+			array('Item' => array('id' => 1, 'syfile_id' => 1, 'published' => 0, 'name' => 'Item 1')),
+			array('Item' => array('id' => 3, 'syfile_id' => 3, 'published' => 0, 'name' => 'Item 3')),
+			array('Item' => array('id' => 4, 'syfile_id' => 4, 'published' => 0, 'name' => 'Item 4')),
+			array('Item' => array('id' => 5, 'syfile_id' => 5, 'published' => 0, 'name' => 'Item 5'))
+		);
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Portfolio->getItems('count');
+		$expected = 4;
+		$this->assertEqual($result, $expected);
+	}
+	
+	function testUnderscoredStyle() {
+		$this->Portfolio->id = 1;
+		$result = $this->Portfolio->get_items();
+		$expected = array(1 => 'Item 1', 3 => 'Item 3', 4 => 'Item 4', 5 => 'Item 5');
+		$this->assertEqual($result, $expected);
+
+		$this->Image->id = 1;
+		$result = $this->Image->get_syfile();
+		$expected = array('Syfile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
+		$this->assertEqual($result, $expected);
+	}
+	
+	function testUninstantiatedModel() {
+		$this->expectException();
+		$this->Image->id = false;
+		$result = $this->Image->getSyfile();
+		$this->assertFalse($result);
+	}
+	
+	function testUnexistantAssociation() {
+		$this->expectException();
+		$this->Image->id = 1;
+		$result = $this->Image->getACherryFromTheTopOfTheCakeForMePlease();
+		$this->assertFalse($result);
+	}
+}
