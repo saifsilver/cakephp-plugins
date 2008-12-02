498a9b6aa2c529df695b151a48eac71349e6718e
rafaelbandeira3 <rafaelbandeira3@gmail.com>
2008-12-02 00:21:07 -0200

    fixing code formatings

--- tests/cases/lazy_loader.test.php
+++ tests/cases/lazy_loader.test.php
@@ -21,7 +21,7 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		'core.syfile', 'core.image', 'core.message',
 		'core.thread', 'core.bid', 'core.project'
 	);
-	
+
 	function start() {
 		parent::start();
 		$this->Portfolio = ClassRegistry::init('Portfolio');
@@ -37,7 +37,7 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		$this->Image->Behaviors->attach('LazyLoader');
 		$this->Message->Behaviors->attach('LazyLoader');
 		$this->Thread->Behaviors->attach('LazyLoader');
-		
+
 		$this->Image->bind('Syfile', array('hasOne'));
 	}
 
@@ -53,34 +53,34 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		$result = $this->Syfile->getImage(true);
 		$this->assertIsA($result, 'Image');
 	}
-	
+
 	function testHasOne() {
 		$this->Image->id = 1;
 		$result = $this->Image->getSyfile();
 		$expected = array('Syfile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testHasOneLoadInstance() {
 		$this->Image->id = 1;
 		$result = $this->Image->getSyfile(true);
 		$this->assertIsA($result, 'Syfile');
 	}
-	
+
 	function testHasAndBelongsToMany() {
 		$this->Portfolio->id = 1;
 		$result = $this->Portfolio->getItems();
 		$expected = array(1 => 'Item 1', 3 => 'Item 3', 4 => 'Item 4', 5 => 'Item 5');
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testHasMany() {
 		$this->Thread->id = 1;
 		$result = $this->Thread->getMessages();
 		$expected = array(1 => 'Thread 1, Message 1');
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testAlternativeFindType() {
 		$this->Portfolio->id = 1;
 		$result = $this->Portfolio->getItems('all');
@@ -96,7 +96,7 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		$expected = 4;
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testUnderscoredStyle() {
 		$this->Portfolio->id = 1;
 		$result = $this->Portfolio->get_items();
@@ -108,14 +108,14 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		$expected = array('Syfile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testUninstantiatedModel() {
 		$this->expectException();
 		$this->Image->id = false;
 		$result = $this->Image->getSyfile();
 		$this->assertFalse($result);
 	}
-	
+
 	function testUnexistantAssociation() {
 		$this->expectException();
 		$this->Image->id = 1;
@@ -129,15 +129,15 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 		$result = $this->Message->getThread();
 		$expected = array('Thread' => array('id' => 1, 'project_id' => 1, 'name' => 'Project 1, Thread 1'));
 		$this->assertEqual($result, $expected);
-		
+
 		$result = $this->Message->getThread(true);
 		$this->assertIsA($result, 'Thread');		
 	}
-	
+
 	function testComposedNameAssociation() {
 		$this->Image->unbindModel(array('hasOne' => array('Syfile')));
 		$this->Image->bind('SystemFile', array('hasOne', 'className' => 'Syfile'), false);
-		
+
 		$this->Image->id = 1;
 		$result = $this->Image->getSystemFile();
 		$expected = array('SystemFile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
@@ -145,17 +145,17 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 
 		$this->Portfolio->unbindModel(array('hasAndBelongsToMany' => array('Item')));
 		$this->Portfolio->bind('RelatedItem', array('hasAndBelongsToMany', 'className' => 'Item'), false);
-		
+
 		$this->Portfolio->id = 1;
 		$result = $this->Portfolio->getRelatedItems();
 		$expected = array(1 => 'Item 1', 3 => 'Item 3', 4 => 'Item 4', 5 => 'Item 5');
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testUnderscoredStyleComposedNameAssociation() {
 		$this->Image->unbindModel(array('hasOne' => array('Syfile')));
 		$this->Image->bind('SystemFile', array('hasOne', 'className' => 'Syfile'), false);
-		
+
 		$this->Image->id = 1;
 		$result = $this->Image->get_system_file();
 		$expected = array('SystemFile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
@@ -163,24 +163,19 @@ class LazyLoaderBehaviorTest extends CakeTestCase {
 
 		$this->Portfolio->unbindModel(array('hasAndBelongsToMany' => array('Item')));
 		$this->Portfolio->bind('RelatedItem', array('hasAndBelongsToMany', 'className' => 'Item'), false);
-		
+
 		$this->Portfolio->id = 1;
 		$result = $this->Portfolio->get_related_items();
 		$expected = array(1 => 'Item 1', 3 => 'Item 3', 4 => 'Item 4', 5 => 'Item 5');
 		$this->assertEqual($result, $expected);
 	}
-	
+
 	function testErroneousRegexpMatch() {
 		$this->Syfile->recursive = -1;
 		$result = $this->Syfile->findById(1);
-		$expected = array('Syfile' => array(
-			'id' => 1,
-			'image_id' => 1,
-			'name' => 'Syfile 1',
-			'item_count' => null
-		));
+		$expected = array('Syfile' => array('id' => 1, 'image_id' => 1, 'name' => 'Syfile 1', 'item_count' => null));
 		$this->assertEqual($result, $expected);
-		
+
 		$this->expectError();
 		$this->Syfile->someMethodThatWillNeverBeHandledAsItIsNotOnlyNotImplementedInTheModelAsItIsNotImplementedInAnyOfItsBehaviors();
 	}
